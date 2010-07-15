62a0eb78f536322591fbbcc2033e9b6b4004dd4b
Satoru SATOH <satoru.satoh@gmail.com>
2010-07-15 11:22:19 +0900

    Fixed a debug message and added copyright notice

--- csvs2xls.py
+++ csvs2xls.py
@@ -1,7 +1,27 @@
 #! /usr/bin/python
 #
-# Author: Satoru SATOH <satoru.satoh at gmail.com>
-# License: MIT
+# Generate an Excel (.xls) file from multiple CSV files.
+#
+# Copyright (C) 2010 Satoru SATOH <satoru.satoh at gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 #
 # Requirements: python, python-xlwt (http://pypi.python.org/pypi/xlwt/)
 #
@@ -157,7 +177,7 @@ class CsvsWorkbook(object):
             threashold0 = 15
 
             for i in range(0, len(dataset[0])):
-                logging.info(" col[i].width = %d" % mcws[i])
+                logging.info(" col[%d].width = %d" % (i, mcws[i]))
                 w = mcws[i]
                 if w < threashold0:
                     w += factor1
