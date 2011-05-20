ba8f5ad28e7bf60bfd7045f733a613f6615a8893
Satoru SATOH <satoru.satoh@gmail.com>
2011-05-20 15:55:41 +0900

    added a cui tool for python-cheetah

--- cheetah-compile.py
+++ cheetah-compile.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 #
-# cheetah-compile.py - enhanced version of 'cheetah' command in python-cheetah.
+# cheetah-compile.py - cui tool to compile template.
 #
 # Copyright (C) 2011 Red Hat, Inc.
 # Red Hat Author(s): Satoru SATOH <ssato@redhat.com>
@@ -29,13 +29,12 @@
 # SEE ALSO: python-cheetah: http://cheetahtemplate.org
 #
 
-import cPickle as pickle
 import optparse
 import os
 import os.path
 import sys
 
-from Cheetah.CheetahWrapper import *
+from Cheetah.Template import Template
 
 ## Defines idata loaders:
 SUPPORTED_DATA_LOADERS = {
@@ -66,10 +65,13 @@ def load_idata(format, path, loaders=SUPPORTED_DATA_LOADERS, **kwargs):
     if load_fun is None:
         sys.stderr.write("[WARN] Could not find any loaders for the requested format: %s\n" % format)
     else:
-        try:
-            return load_fun(path)
-        except Exception, e:
-            sys.stderr.write("Error (%s): %s\n" % (repr(e.__class__), str(e)))
+        ## FIXME: It might be better to handle exceptions.
+        #try:
+        #    return load_fun(path)
+        #except Exception, e:
+        #    sys.stderr.write("Error during loading %s: (%s): %s\n" % (path, repr(e.__class__), str(e)))
+        #    ...
+        return load_fun(path)
 
     return {}
 
@@ -98,102 +100,63 @@ def parse_idata_option(optstr, check_exists=False):
     return ()
 
 
+def compile_template(template, params, is_file=False):
+    """
+    TODO: Add test case that $template is a filename.
 
-class ExtCheetahWrapper(CheetahWrapper):
+    >>> tmpl_s = "a=$a b=$b"
+    >>> params = {'a':1, 'b':'b'}
+    >>> 
+    >>> assert "a=1 b=b" == compile_template(tmpl_s, params)
+    """
+    if is_file:
+        tmpl = Template(file=template, searchList=params)
+    else:
+        tmpl = Template(source=template, searchList=params)
 
-    idata = {}
-
-    ##################################################
-    ## Modified version of CheetahWrapper.parseOpts:
-
-    def parseOpts(self, args):
-        C, D, W = self.chatter, self.debug, self.warn
-        self.isCompile = isCompile = self.command[0] == 'c'
-        defaultOext = isCompile and ".py" or ".html"
-        self.parser = OptionParser()
-        pao = self.parser.add_option
-        pao("--idir", action="store", dest="idir", default='', help='Input directory (defaults to current directory)')
-        pao("--odir", action="store", dest="odir", default="", help='Output directory (defaults to current directory)')
-        pao("--iext", action="store", dest="iext", default=".tmpl", help='File input extension (defaults: compile: .tmpl, fill: .tmpl)')
-        pao("--oext", action="store", dest="oext", default=defaultOext, help='File output extension (defaults: compile: .py, fill: .html)')
-        pao("-R", action="store_true", dest="recurse", default=False, help='Recurse through subdirectories looking for input files')
-        pao("--stdout", "-p", action="store_true", dest="stdout", default=False, help='Send output to stdout instead of writing to a file')
-        pao("--quiet", action="store_false", dest="verbose", default=True, help='Do not print informational messages to stdout')
-        pao("--debug", action="store_true", dest="debug", default=False, help='Print diagnostic/debug information to stderr')
-        pao("--env", action="store_true", dest="env", default=False, help='Pass the environment into the search list')
-        pao("--pickle", action="store", dest="pickle", default="", help='Unpickle FILE and pass it through in the search list')
-        pao("--flat", action="store_true", dest="flat", default=False, help='Do not build destination subdirectories')
-        pao("--nobackup", action="store_true", dest="nobackup", default=False, help='Do not make backup files when generating new ones')
-        pao("--settings", action="store", dest="compilerSettingsString", default=None, help='String of compiler settings to pass through, e.g. --settings="useNameMapper=False,useFilters=False"')
-        pao('--print-settings', action='store_true', dest='print_settings', help='Print out the list of available compiler settings')
-        pao("--templateAPIClass", action="store", dest="templateClassName", default=None, help='Name of a subclass of Cheetah.Template.Template to use for compilation, e.g. MyTemplateClass')
-        pao("--parallel", action="store", type="int", dest="parallel", default=1, help='Compile/fill templates in parallel, e.g. --parallel=4')
-        pao('--shbang', dest='shbang', default='#!/usr/bin/env python', help='Specify the shbang to place at the top of compiled templates, e.g. --shbang="#!/usr/bin/python2.6"')
-        
-        # Added: 
-        pao("--idata", action="store", dest="idata", default="",
-            help='Data format and path to pass it through in the search list, e.g. pickle:data.pkl, json:../data.json.')
-
-        opts, files = self.parser.parse_args(args)
-        self.opts = opts
-        if sys.platform == "win32":
-            new_files = []
-            for spec in files:
-                file_list = glob.glob(spec)
-                if file_list:
-                    new_files.extend(file_list)
-                else:
-                    new_files.append(spec)
-            files = new_files
-        self.pathArgs = files
-
-        D("""\
-cheetah compile %s
-Options are
-%s
-Files are %s""", args, pprint.pformat(vars(opts)), files)
+    return tmpl.respond()
 
 
-        if opts.print_settings:
-            print() 
-            print('>> Available Cheetah compiler settings:')
-            from Cheetah.Compiler import _DEFAULT_COMPILER_SETTINGS
-            listing = _DEFAULT_COMPILER_SETTINGS
-            listing.sort(key=lambda l: l[0][0].lower())
+def main(argv=sys.argv):
+    defaults = {
+        "output": "",
+        "idata": "",
+    }
 
-            for l in listing:
-                print('\t%s (default: "%s")\t%s' % l)
-            sys.exit(0)
+    p = optparse.OptionParser("%prog [OPTION ...] TEMPLATE_FILE")
+    p.set_defaults(**defaults)
 
-        #cleanup trailing path separators
-        seps = [sep for sep in [os.sep, os.altsep] if sep]
-        for attr in ['idir', 'odir']:
-            for sep in seps:
-                path = getattr(opts, attr, None)
-                if path and path.endswith(sep):
-                    path = path[:-len(sep)]
-                    setattr(opts, attr, path)
-                    break
+    p.add_option("-o", "--output", help="Output file")
+    p.add_option("", "--idata", 
+        help="Data format and path to pass it through in the search list, e.g. pickle:data.pkl, json:../data.json.")
 
-        self._fixExts()
-        if opts.env:
-            self.searchList.insert(0, os.environ)
-        if opts.pickle:
-            f = open(opts.pickle, 'rb')
-            unpickled = pickle.load(f)
-            f.close()
-            self.searchList.insert(0, unpickled)
+    (opts, args) = p.parse_args()
 
-        if opts.idata:
-            fmt_and_path = parse_idata_option(opts.idata)
-            if fmt_and_path:
-                self.idata = load_idata(*fmt_and_path)
-                self.searchList.append(self.idata)
+    if not args:
+        p.print_usage()
+        sys.exit(1)
 
+    template = args[0]
+    params = {}
+
+    output = opts.output and open(opts.output, "w") or sys.stdout
+
+    if opts.idata:
+        fmt_and_path = parse_idata_option(opts.idata)
+
+        if fmt_and_path:
+            idata = load_idata(*fmt_and_path)
+            params = idata
+            #import pprint; pprint.pprint(params)
+
+    res = compile_template(template, params, is_file=True)
+    print >> output, res
+
+    sys.exit()
 
 
 if __name__ == '__main__':
-    ExtCheetahWrapper().main()
+    main()
 
 
 # vim: shiftwidth=4 tabstop=4 expandtab:
