159212ef4ddb34eeac3aea0ef1487d8f58700155
Satoru SATOH <satoru.satoh@gmail.com>
2014-02-20 08:01:24 +0900

    add an init script example w/ application tests

---
+++ init_script_example.sh
@@ -0,0 +1,196 @@
+#!/bin/bash
+#
+# Init script script example.
+#
+# Copyright (C) 2014 Red Hat, Inc.
+# Author: Satoru SATOH <ssato redhat.com>
+# License: MIT
+#
+# Install:
+#   1. Modify this script to fit with your application or something and your
+#      purpose. Do not forget test it by run it with 'test' argument also.
+#
+#   2. Install it in /etc/rc.d/init.d/ with appropriate attributes (permission,
+#      owner, group and SELinux label, etc.) and name represents the
+#      application or something you want to control with this script like the
+#      followings:
+#       
+#      # install -m 755 <this script> /etc/rc.d/init.d/<application_name>
+#      # restorecon -rv /etc/rc.d/init.d/
+#
+# chkconfig: - 85 15
+# description: Init script example
+# processname: <enter_your_app_process_etc>
+# config: /etc/sysconfig/<enter_your_app_name_etc>
+# pidfile: /var/run/<enter_your_app_name_etc>.pid
+#
+### BEGIN INIT INFO
+# Provides: <enter_your_app_name_etc>
+# Required-Start: $local_fs $remote_fs $network $named
+# Required-Stop: $local_fs $remote_fs $network
+# Short-Description: Init script example
+# Description: Init script example with tests
+### END INIT INFO
+
+# Example session log:
+#
+# ssato@localhost% ln -s init_script_example.sh foo
+# ssato@localhost% PIDFILE=/tmp/foo.pid LOCKFILE=/tmp/foo.lock ./foo; echo $?
+# Usage: foo {start|stop|status|restart|condrestart|try-restart
+# 2
+# ssato@localhost% PIDFILE=/tmp/foo.pid LOCKFILE=/tmp/foo.lock ./foo start; echo $?
+# Starting foo: 
+# 0
+# ssato@localhost% PIDFILE=/tmp/foo.pid LOCKFILE=/tmp/foo.lock ./foo status; echo $?
+# 0
+# ssato@localhost% PIDFILE=/tmp/foo.pid LOCKFILE=/tmp/foo.lock ./foo stop; echo $?
+# Stopping foo: 
+# 0
+# ssato@localhost% PIDFILE=/tmp/foo.pid LOCKFILE=/tmp/foo.lock ./foo status; echo $?
+# 3
+# ssato@localhost% ./foo test
+# Setup before tests of 'foo': ensure ./foo is stopped ...        OK
+# Check if to start from stopped state exits with exit code 0 ... OK
+# Check if to start from started state exits with exit code 0 ... OK
+# Check if to restart from started state exits with exit code 0 ...       Stopping foo: 
+# Starting foo: 
+# OK
+# Check if 'status' on started state exits with exit code 0 ...   OK
+# Check if to stop from started state exits with exit code 0 ...  Stopping foo: 
+# OK
+# Check if 'status' on stopped state exits with exit code 3 ...   OK
+# Check if to stop from stopped state exits with exit code 0 ...  OK
+# Check if to restart from stopped state exits with exit code 0 ...       Starting foo: 
+# OK
+# ssato@localhost%
+
+# Edit and customize this:
+#MY_APP_NAME=foo
+MY_APP_NAME=${0##*/}
+
+. /etc/rc.d/init.d/functions
+test -f /etc/sysconfig/${MY_APP_NAME} && . /etc/sysconfig/${MY_APP_NAME}
+
+# Start foo in the C locale by default.
+MY_APP_LANG=${MY_APP_LANG-"C"}
+
+prog=${MY_APP_NAME}  # e.g. /usr/sbin/httpd
+pidfile=${PIDFILE-/var/run/foo.pid}
+lockfile=${LOCKFILE-/var/lock/subsys/foo}
+STOP_TIMEOUT=${STOP_TIMEOUT-10}
+
+start () {
+    echo -n $"Starting $prog: "
+    # There is no '${MY_APP_NAME}' daemon exists in actual as this script is
+    # just an example so the line below must be commented:
+    #LANG=$MY_APP_LANG daemon --pidfile=${pidfile} ${prog} $OPTIONS
+    LANG=$MY_APP_LANG touch ${pidfile}  # Just create empty PID file instead.
+    local rc=$?
+    echo
+    [ $rc = 0 ] && touch ${lockfile}
+    return $rc
+}
+
+# When stopping foo, a delay (of default 10 second) is required before
+# SIGKILLing the foo parent; this gives enough time for the foo parent to
+# SIGKILL any errant children.
+stop() {
+	echo -n $"Stopping $prog: "
+	# Likewise. See the comment in 'start' shell function also.
+	#killproc -p ${pidfile} -d ${STOP_TIMEOUT} $foo
+	local rc=$?
+	echo
+	[ $rc = 0 ] && rm -f ${lockfile} ${pidfile}
+}
+
+my_status () {
+    #status -p ${pidfile} $foo; rc=$?
+    # if test $rc -eq 0; then
+    test -f ${lockfile} && return 0 || return 3
+    # else
+    #     exit 1
+    # fi
+}
+
+function test_helper () {
+    local expected_rc=$1
+    local test_desc="$2"
+    local test_body="$3"
+
+    echo -ne "Check if ${test_desc:?} exits with exit code ${expected_rc:?} ...\t"
+    eval "${test_body:?}"; rc=$?
+    if test $rc -ne ${expected_rc}; then
+        echo "Failed. rc=$rc"
+        exit -1
+    else
+        echo "OK"
+    fi
+}
+
+# system (application) tests.
+# see also: http://refspecs.linuxbase.org/LSB_3.1.1/LSB-Core-generic/LSB-Core-generic/iniscrptact.html
+system_test () {
+    echo -ne "Setup before tests of '$MY_APP_NAME': ensure $0 is stopped ...\t"
+    $0 stop
+    if test $? -ne 0; then
+        echo "Failed to stop $0. Aborting..."
+        exit -1
+    fi
+    echo "OK"
+
+    test_env="PIDFILE=/tmp/${MY_APP_NAME}.pid LOCKFILE=/tmp/${MY_APP_NAME} VERBOSE='0'"
+    test_helper 0 "to start from stopped state" "${test_env} $0 start"
+    test_helper 0 "to start from started state" "${test_env} $0 start"
+    test_helper 0 "to restart from started state" "${test_env} $0 restart"
+    test_helper 0 "'status' on started state" "${test_env} $0 status"
+    test_helper 0 "to stop from started state" "${test_env} $0 stop"
+    test_helper 3 "'status' on stopped state" "${test_env} $0 status"
+    test_helper 0 "to stop from stopped state" "${test_env} $0 stop"
+    test_helper 0 "to restart from stopped state" "${test_env} $0 restart"
+    $0 stop
+}
+
+case "$1" in
+  start)
+        my_status; rc=$?
+        if test $rc -eq 3; then  # It's stopped.
+            start; rc=$?
+        fi
+        ;;
+  stop)
+        my_status; rc=$?
+        if test $rc -eq 0; then  # It's running.
+            stop; rc=$?
+        else
+            rc=0
+        fi
+        ;;
+  status)
+        my_status; rc=$?
+        ;;
+  restart)
+        my_status; rc=$?
+        if test $rc -eq 3; then
+            start; rc=$?
+        elif test $rc -eq 0; then
+            stop && start; rc=$?
+        fi
+        ;;
+  condrestart|try-restart)
+        my_status; rc=$?
+        if test $rc -eq 0; then
+            stop && start; rc=$?
+        else
+            rc=0
+        fi
+        ;;
+  test|systemtest)
+        system_test; rc=$?
+        ;;
+  *)
+        echo $"Usage: $prog (start|stop|status|restart|condrestart|try-restart)"
+        rc=1
+esac
+exit $rc
+
+# vim:sw=4:ts=4:et:
