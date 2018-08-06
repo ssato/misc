511803c1cc36cdefa439fe28a1a73b78e46beee0
Satoru SATOH <satoru.satoh@gmail.com>
2018-08-06 14:26:28 +0900

    enhancement: allow to specify copr project

--- rpmspecs/copr-build.sh
+++ rpmspecs/copr-build.sh
@@ -5,19 +5,19 @@
 # - https://developer.fedoraproject.org/deployment/copr/copr-cli.html
 # - http://copr-rest-api.readthedocs.io/en/latest/Resources/build.html#submit-new-build
 #
-set -ex
-
-copr_project=ssato/python-anyconfig
+set -e
 
 curdir=${0%/*}
 topdir=${curdir}/../
 srpm=$1
+copr_project=${2:-ssato/python-anyconfig}
 
 if test "x${srpm}" = "x"; then
-    echo "Usage: $0 SRPM_PATH"
+    echo "Usage: $0 SRPM_PATH [COPR_PROJECT]"
     exit -1
 fi
 
+set -x
 test -f ~/.config/copr
 copr-cli build ${copr_project:?} ${srpm:?}
 
