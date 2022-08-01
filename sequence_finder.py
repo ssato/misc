449957ef26b5f9fb88c3187de965fbad777c3b9b
Satoru SATOH <satoru.satoh@gmail.com>
2022-08-02 04:35:41 +0900

    fix: process cases if not found

--- sequence_finder.py
+++ sequence_finder.py
@@ -32,6 +32,7 @@ ssato@fedora%
 """
 import os
 import random
+import sys
 import typing
 
 import click
@@ -40,6 +41,10 @@ import click
 def sequence_(*keys: str) -> str:
     while True:
         yield random.choice(keys)
+
+
+class FoundExc(Exception):
+    pass
 
 
 def find_a_needle_in_a_haystack_itr(
@@ -55,20 +60,24 @@ def find_a_needle_in_a_haystack_itr(
     for item in haystack:
         yield item
 
-        if acc and item == last:
-            if acc == lead:
-                return
-            else:
-                acc.append(item)
-                continue
-
         if item == head:
             acc = [head]
+            continue
+
+        if acc:
+            if item == last and acc == lead:
+                raise FoundExc()
+
+            acc.append(item)
+
+
+NEEDLE: str = 'ドド スコ スコ スコ'
+MSG: str = 'ラブ注入♡ '
 
 
 @click.command()
 @click.option(
-    '--needle', default='ドド スコ スコ スコ',
+    '--needle', default=NEEDLE,
     help="A sequence of words separated with ' ' (a space) to search"
 )
 @click.option(
@@ -76,34 +85,37 @@ def find_a_needle_in_a_haystack_itr(
     help="A list of words separated with ' ' (a space) to search from"
 )
 @click.option(
-    '--msg', default='ラブ注入♡',
-    help="A message to print if it was found"
+    '--msg', default=MSG, help="A message to print if it was found"
 )
 @click.option(
     '--verbose', '-v', is_flag=True, default=False, help="Verbose mode"
 )
 def main(
-    needle: str,
-    haystack: str,
-    msg: str,
-    verbose: bool
+    needle: str = NEEDLE,
+    haystack: str = '',
+    msg: str = MSG,
+    verbose: bool = False
 ) -> None:
     """entry point."""
     ndl = needle.split()
     hstk = haystack.split() if haystack else sequence_(*set(ndl))
     vitr = find_a_needle_in_a_haystack_itr(hstk, ndl)
 
-    if verbose:
-        nitems: int = 0
+    try:
+        nitems = 0
         for val in vitr:
             print(f'{val}', end=' ')
             nitems += 1
 
-        pos: int = nitems - len(ndl) + 1
-        print(f'{os.linesep}{msg}  (at: {pos})')
-    else:
-        print(' '.join(vitr))
-        print(msg)
+        print(f'{os.linesep}Not found: {needle}')
+        sys.exit(1)
+
+    except FoundExc:
+        if verbose:
+            pos: int = nitems - len(ndl) + 1
+            print(f'{os.linesep}{msg} (at: {pos})')
+        else:
+            print(f'{os.linesep}{msg}')
 
 
 if __name__ == '__main__':
